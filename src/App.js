6eebf275f8f9dc9a19b542e62cd903b01df0f6fc
manorama1234 <mohantym31@gmail.com>
2022-03-21 18:38:44 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Greet from "./Greet";
+class App extends React.Component{
+  render(){
+   return(
+     <>
+     <h1>my app Component</h1>
+     <Greet/>
+     </>
+   )
+  }
+}
+
+export default App
---
+++ src/Greet.js
@@ -0,0 +1,11 @@
+import React from "react"
+ class Greet extends React .Component{
+     render(){
+        return(
+         <div>
+            <h1>good morning</h1>
+         </div>
+         )
+     }
+ }
+ export default Greet
